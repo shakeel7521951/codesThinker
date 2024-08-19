bae342047506907102ce4ee2a754f6992fcdac87
Imranf620 <imranf620@gmail.com>
2024-08-19 01:38:46 -0700

    Complete

---
+++ src/Componets/Pages/Home/Home1/TeamSection/teamMembers.jsx
@@ -0,0 +1,132 @@
+
+import team1 from "../../../../images/wasem.png";
+import team2 from "../../../../images/usama.png";
+import team3 from "../../../../images/zulfiqar.png";
+import team4 from "../../../../images/Asad.png";
+import team5 from "../../../../images/imran.png";
+import team6 from "../../../../images/bushra.png";
+const teamMembers = [
+    {
+       id:1,
+      image: team1,
+      name: "Waseem Malik",
+      role: "C.E.O",
+     
+      socialLinks: {
+        facebook: "#",
+        twitter: "#",
+        linkedin: "#",
+      },
+    },
+    {
+      id:2,
+      image: team2,
+      name: "Usama joiya",
+      role: "C.E.O",
+      Experience:"10 years",
+      phone:"030799456585",
+      email:"Ali@gmail.com",
+      personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role",
+      socialLinks: {
+      twitter: '#',
+        facebook: 'https://www.facebook.com/',
+        linkedin: 'https://www.linkedin.com/',
+      },
+    },
+    {
+      id:3,
+      image: team3,
+      name: "Zulfiqar Malik",
+      role: "C.F.O",
+       work:"Mern Stack Developer",
+      Experience:"10 years",
+      phone:"030799456585",
+      email:"Ali@gmail.com",
+      personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role",
+      socialLinks: {
+      twitter: '#',
+        facebook: 'https://www.facebook.com/',
+        linkedin: 'https://www.linkedin.com/',
+      },
+    },
+   
+       
+      {
+        id:4,
+        image: team4,
+        name: "AsadUllah",
+        role: "GM",
+        Experience:"10 years",
+        phone:"030799456585",
+        email:"Ali@gmail.com",
+        personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role ",
+        socialLinks: {
+        twitter: '#',
+          facebook: 'https://www.facebook.com/',
+          linkedin: 'https://www.linkedin.com/',
+        },
+        },
+        {
+          id:5,
+          image: team5,
+          name: "Muhammad Imran",
+          role: "Mern Stack Developer",
+          Experience:"10 years",
+          phone:"030799456585",
+          email:"Ali@gmail.com",
+          personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role ",
+          socialLinks: {
+          twitter: '#',
+            facebook: 'https://www.facebook.com/',
+            linkedin: 'https://www.linkedin.com/',
+          },
+          },
+          {
+            id:6,
+            image: team6,
+            name: "Bushra Maham",
+            role: "Mern Stack Developer",
+            Experience:"10 years",
+            phone:"03222105603",
+            email:"bushramaham2002@gmail.com",
+            personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role ",
+            socialLinks: {
+            twitter: '#',
+              facebook: 'https://www.facebook.com/',
+              linkedin: 'https://www.linkedin.com/',
+            },
+            },
+         
+            {
+              id:7,
+              image: "",
+              name: "Allah Nawaz",
+              role: "Mern Stack Developer",
+              Experience:"10 years",
+              phone:"03222105603",
+              email:"bushramaham2002@gmail.com",
+              personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role ",
+              socialLinks: {
+              twitter: '#',
+                facebook: 'https://www.facebook.com/',
+                linkedin: 'https://www.linkedin.com/',
+              },
+              },
+              {
+                id:6,
+                image: "",
+                name: "Abdul Razaq",
+                role: "App Developer",
+                Experience:"10 years",
+                phone:"03222105603",
+                email:"bushramaham2002@gmail.com",
+                personalinfo:"A personal profile, or personal statement, CV profile or career aim, is basically an overview of your career portfolio. It should be a small paragraph that sits at the top of your CV, and is your chance to showcase who you are, along with your skills and strengths relevant to your specific sector or job role ",
+                socialLinks: {
+                twitter: '#',
+                  facebook: 'https://www.facebook.com/',
+                  linkedin: 'https://www.linkedin.com/',
+                },
+                },
+  ];
+
+  export default teamMembers
